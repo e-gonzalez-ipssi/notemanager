8ee2004a2c1f79764013d8fbdbc7b89386f6a998
NicolasDENIS <n.denis@ecole-ipssi.net>
2022-06-29 17:15:48 +0200

    @Dev: Adding ScrollView

--- src/features/Home.tsx
+++ src/features/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Text } from "react-native";
+import { ScrollView, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import SearchBar from "../component/BasicTextInput";
 import ThemeButton from "../component/ThemeButton";
@@ -27,14 +27,16 @@ export default function Home({ navigation }: { navigation: any }) {
 
     return (
         <SafeAreaView style={{ flex: 1, marginTop: 20 }}>
-            <ThemeButton />
-            <Text style={{ color: color.text }}>
-                Page Home
-            </Text>
-            <SearchBar setInput={setSearch} />
-            {notes.map((note, index) => {
-                return <Text key={index}>{note.title}</Text>
-            })}
+            <ScrollView>
+                <ThemeButton />
+                <Text style={{ color: color.text }}>
+                    Page Home
+                </Text>
+                <SearchBar setInput={setSearch} />
+                {notes.map((note, index) => {
+                    return <Text key={index}>{note.title}</Text>
+                })}
+            </ScrollView>
         </SafeAreaView>
     )
 }
--- src/features/New.tsx
+++ src/features/New.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Text } from "react-native";
+import { ScrollView, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import useTheme from "../hooks/theme";
 import ImageSelector from "../component/ImageSelector";
@@ -47,66 +47,68 @@ export default function New({ navigation }: { navigation: any }) {
 
     return (
         <SafeAreaView style={{ flex: 1, marginTop: 20 }}>
-            <Text>
-                Page New
-            </Text>
-            <Text
-                style={{ textAlign: "center" }}
-            >
-                Auteur
-            </Text>
-            <BasicTextInput
-                setInput={setAuteur}
-                placeholder={user === "" ? "Enter your name" : user}
-            />
-            <BasicCheckBox
-                style={{
-                    margin: 10
-                }}
-                onValueChange={setAnonymous}
-                value={anonymous}
-            >
-                Post as Anonymous
-            </BasicCheckBox>
-            <Text
-                style={{ textAlign: "center" }}
-            >
-            </Text>
-            <BasicTextInput
-                setInput={setTitre}
-                placeholder="Ex: Cours de React Native"
-            />
-            <Text
-                style={{ textAlign: "center" }}
-            >
-                Texte
-            </Text>
-            <BasicTextInput
-                setInput={setTexte}
-                placeholder="Ex: Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-            mollit anim id est laborum."
-                style={{ height: 200, width: 340 }}
-                multiline={true}
-            />
-            <Text
-                style={{ textAlign: "center" }}
-            >
-                Tags
-            </Text>
-            <BasicTextInput
-                setInput={handleTags}
-                placeholder="Ex: Pain au chocolat"
-            />
-            <ImageSelector
-                onImage={setImage}
-            />
-            <BasicButton onPress={handleConfirm} >
-                Créer la note
-            </BasicButton>
+            <ScrollView>
+                <Text>
+                    Page New
+                </Text>
+                <Text
+                    style={{ textAlign: "center" }}
+                >
+                    Auteur
+                </Text>
+                <BasicTextInput
+                    setInput={setAuteur}
+                    placeholder={user === "" ? "Enter your name" : user}
+                />
+                <BasicCheckBox
+                    style={{
+                        margin: 10
+                    }}
+                    onValueChange={setAnonymous}
+                    value={anonymous}
+                >
+                    Post as Anonymous
+                </BasicCheckBox>
+                <Text
+                    style={{ textAlign: "center" }}
+                >
+                </Text>
+                <BasicTextInput
+                    setInput={setTitre}
+                    placeholder="Ex: Cours de React Native"
+                />
+                <Text
+                    style={{ textAlign: "center" }}
+                >
+                    Texte
+                </Text>
+                <BasicTextInput
+                    setInput={setTexte}
+                    placeholder="Ex: Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+                mollit anim id est laborum."
+                    style={{ height: 200, width: 340 }}
+                    multiline={true}
+                />
+                <Text
+                    style={{ textAlign: "center" }}
+                >
+                    Tags
+                </Text>
+                <BasicTextInput
+                    setInput={handleTags}
+                    placeholder="Ex: Pain au chocolat"
+                />
+                <ImageSelector
+                    onImage={setImage}
+                />
+                <BasicButton onPress={handleConfirm} >
+                    Créer la note
+                </BasicButton>
+            </ScrollView>
         </SafeAreaView>
     )
 }
