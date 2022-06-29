e2f9c0b860552591c97634970c48aabe7e605aa3
Esteban Gonzalez <44523139+frisco78es@users.noreply.github.com>
2022-06-29 14:21:01 +0200

    Checkbox part 2

--- src/features/New.tsx
+++ src/features/New.tsx
@@ -1,20 +1,28 @@
-import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import useTheme from "../hooks/theme";
 import ImageSelector from "../component/ImageSelector";
+import BasicCheckBox from "../component/BasicCheckbox";
 
 export default function New({ navigation }: { navigation: any }) {
-    const {color}=useTheme();
+    const { color } = useTheme();
+    const [anonymous, setAnonymous] = useState(false);
     return (
-        <SafeAreaView style={{flex: 1, marginTop:20}}>
+        <SafeAreaView style={{ flex: 1, marginTop: 20 }}>
             <Text>
                 Page New
             </Text>
-            <Text onPress={() => { navigation.navigate("Detail") }}>
-                Aller sur la page Detail
-            </Text>
             <ImageSelector onImage={() => { }} />
+            <BasicCheckBox
+                style={{
+                    margin: 10
+                }}
+                onValueChange={setAnonymous}
+                value={anonymous}
+            >
+                Post as Anonymous
+            </BasicCheckBox>
         </SafeAreaView>
     )
 }
