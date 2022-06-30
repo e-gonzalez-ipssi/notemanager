ca2b67b2df56f939feedf3f19c5879acca6902cf
Esteban Gonzalez <44523139+frisco78es@users.noreply.github.com>
2022-06-30 11:28:12 +0200

    add tags displayer

--- src/component/NoteData.tsx
+++ src/component/NoteData.tsx
@@ -1,21 +1,14 @@
 import React from "react";
 import { Note } from "../utils/note";
 import { Text, View } from "react-native";
+import Tags from "./Tags";
 
 export default function NoteData({ note }: { note: Note }) {
 	return (
 		<View>
 			<Text>{note.anonym ? "Anonymes" : note.author}</Text>
 			<Text>{note.text}</Text>
-			<View style={{ flexDirection: "row" }}>
-				{note.tags.map((tag, index) => {
-					return (
-						<Text style={{ marginRight: 5 }} key={index}>
-							{tag}
-						</Text>
-					);
-				})}
-			</View>
+			<Tags tags={note.tags} />
 			<Text>{note.creation_date}</Text>
 		</View>
 	);
