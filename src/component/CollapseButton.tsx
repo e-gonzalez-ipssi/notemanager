9a4abb4fe1da034e2698ea265b3ad5fe94e45225
Esteban Gonzalez <44523139+frisco78es@users.noreply.github.com>
2022-06-30 10:14:11 +0200

    NoteTilte + some graphic change

--- src/component/CollapseButton.tsx
+++ src/component/CollapseButton.tsx
@@ -1,64 +1,75 @@
-import { StatusBar } from "expo-status-bar";
-import React, { Component, useState } from "react";
-import { ScrollView, Text, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+import { ScrollView, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StyleSheet } from "react-native";
 import NoteData from "./NoteData";
 import { Note } from "../utils/note";
+import NoteTitle from "./NoteTitle";
+import useTheme from "../hooks/theme";
 
 export default function CollapseButton({ notes = [] }: { notes: Note[] }) {
-  const [currentIndex, setCurrentIndex] = useState<number | null>(null);
+	const { color } = useTheme();
+	const [currentIndex, setCurrentIndex] = useState<number | null>(null);
 
-  const noteDisplay = [] as { Titre: string; Content: React.ReactNode }[];
+	const noteDisplay = [] as { title: React.ReactNode; content: React.ReactNode }[];
 
-  notes.forEach((note) => {
-    noteDisplay.push({
-      Titre: note.title,
-      Content: <NoteData note={note} />,
-    });
-  });
+	notes.forEach((note) => {
+		noteDisplay.push({
+			title: <NoteTitle note={note} />,
+			content: <NoteData note={note} />,
+		});
+	});
 
-  return (
-    <SafeAreaView>
-      <ScrollView>
-        <StatusBar hidden />
-        {noteDisplay.map((note, index) => {
-          return (
-            <TouchableOpacity
-              key={index}
-              onPress={() => {
-                setCurrentIndex(index === currentIndex ? null : index);
-              }}
-            >
-              <View style={styles.notestyle}>
-                <Text key={index} style={styles.header}>
-                  {note.Titre}
-                </Text>
-                {index === currentIndex && (
-                  <View style={styles.contenu}>{note.Content}</View>
-                )}
-              </View>
-            </TouchableOpacity>
-          );
-        })}
-      </ScrollView>
-    </SafeAreaView>
-  );
+	return (
+		<SafeAreaView
+			style={{
+				alignItems: 'center',
+				justifyContent: 'center',
+			}}
+		>
+			<ScrollView>
+				{noteDisplay.map((note, index) => {
+					return (
+						<TouchableOpacity
+							key={index}
+							style={{
+								backgroundColor: color.secondary,
+								width: 350,
+								margin: 10,
+								borderRadius: 16,
+							}}
+							onPress={() => {
+								setCurrentIndex(index === currentIndex ? null : index);
+							}}
+						>
+							<View style={styles.notestyle}>
+								{note.title}
+								{index === currentIndex && (
+									<View style={styles.contenu}>{note.content}</View>
+								)}
+							</View>
+						</TouchableOpacity>
+					);
+				})}
+			</ScrollView>
+		</SafeAreaView >
+	);
 }
 
 const styles = StyleSheet.create({
-  header: {
-    fontSize: 38,
-    fontWeight: "900",
-  },
-  notestyle: {
-    alignItems: "center",
-    padding: 20,
-  },
-  contenu: {},
-  bodycontent: {
-    fontSize: 20,
-    lineHeight: 20 * 1.5,
-    textAlign: "center",
-  },
+	header: {
+		fontSize: 38,
+		fontWeight: "900",
+	},
+	notestyle: {
+		alignItems: "center",
+		padding: 20,
+		justifyContent: 'center'
+	},
+	contenu: {},
+	bodycontent: {
+		fontSize: 20,
+		lineHeight: 20 * 1.5,
+		textAlign: "center",
+	},
 });
--- src/component/NoteData.tsx
+++ src/component/NoteData.tsx
@@ -1,24 +1,22 @@
 import React from "react";
 import { Note } from "../utils/note";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function NoteData({ note }: { note: Note }) {
-  console.log(note);
-
-  return (
-    <View>
-      <Text>{note.anonym ? "Anonymes" : note.author}</Text>
-      <Text>{note.text}</Text>
-      <View style={{ flexDirection: "row" }}>
-        {note.tags.map((tag, index) => {
-          return (
-            <Text style={{ marginRight: 5 }} key={index}>
-              {tag}
-            </Text>
-          );
-        })}
-      </View>
-      <Text>{note.creation_date}</Text>
-    </View>
-  );
+	return (
+		<View>
+			<Text>{note.anonym ? "Anonymes" : note.author}</Text>
+			<Text>{note.text}</Text>
+			<View style={{ flexDirection: "row" }}>
+				{note.tags.map((tag, index) => {
+					return (
+						<Text style={{ marginRight: 5 }} key={index}>
+							{tag}
+						</Text>
+					);
+				})}
+			</View>
+			<Text>{note.creation_date}</Text>
+		</View>
+	);
 }
