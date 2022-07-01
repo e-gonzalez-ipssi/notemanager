5ab58fcc4197763c8a6147be674aaa70df9aee12
Esteban Gonzalez <44523139+frisco78es@users.noreply.github.com>
2022-07-01 12:28:04 +0200

    Update filtre author

--- src/features/Home.tsx
+++ src/features/Home.tsx
@@ -7,84 +7,84 @@ import CustomScrollView from "../component/CustomScrollView";
 import useNotes from "../hooks/notes";
 
 export default function Home({ navigation }: { navigation: any }) {
-  const { notes, fetchData } = useNotes();
-  const [filtreName, setFiltreName] = useState("");
-  const [filtreTag, setFiltreTag] = useState([] as string[]);
-  const [filtreDate, setFiltreDate] = useState("");
+	const { notes, fetchData } = useNotes();
+	const [filtreName, setFiltreName] = useState("");
+	const [filtreTag, setFiltreTag] = useState([] as string[]);
+	const [filtreDate, setFiltreDate] = useState("");
 
-  const handleTags = (rawValue: string) => {
-    if (rawValue.length === 0) {
-      setFiltreTag([]);
-      return;
-    }
+	const handleTags = (rawValue: string) => {
+		if (rawValue.length === 0) {
+			setFiltreTag([]);
+			return;
+		}
 
-    const arrayofTags = rawValue.split(",").map((item) => item.trim());
-    setFiltreTag(arrayofTags);
-  };
+		const arrayofTags = rawValue.split(",").map((item) => item.trim());
+		setFiltreTag(arrayofTags);
+	};
 
-  let filteredNotes = [];
+	let filteredNotes = [];
 
-  filteredNotes = notes.filter((note) => {
-    if (note.author && note.anonym === false) {
-      return note.author.toLowerCase().includes(filtreName.toLowerCase());
-    }
-    return false;
-  });
+	filteredNotes = notes.filter((note) => {
+		if (note.author && note.anonym === false) {
+			return note.author.toLowerCase().includes(filtreName.toLowerCase());
+		}
+		return false;
+	});
 
-  filteredNotes = filteredNotes.filter((note) => {
-    return note.creation_date.includes(filtreDate);
-  });
+	filteredNotes = filteredNotes.filter((note) => {
+		return note.creation_date.includes(filtreDate);
+	});
 
-  if (filtreTag.length !== 0) {
-    filteredNotes = filteredNotes.filter((note) => {
-      var BreakException = {};
+	if (filtreTag.length !== 0) {
+		filteredNotes = filteredNotes.filter((note) => {
+			var BreakException = {};
 
-      try {
-        let filtred = true;
-        filtreTag.forEach((tag) => {
-          const lowerCase = note.tags.map((tag) => tag.toLowerCase());
-          filtred = lowerCase.includes(tag.toLowerCase());
-          if (!filtred) {
-            throw BreakException;
-          }
-        });
-        return filtred;
-      } catch (e) {
-        if (e !== BreakException) throw e;
-      }
-    });
-  }
+			try {
+				let filtred = true;
+				filtreTag.forEach((tag) => {
+					const lowerCase = note.tags.map((tag) => tag.toLowerCase());
+					filtred = lowerCase.includes(tag.toLowerCase());
+					if (!filtred) {
+						throw BreakException;
+					}
+				});
+				return filtred;
+			} catch (e) {
+				if (e !== BreakException) throw e;
+			}
+		});
+	}
 
-  return (
-    <SafeAreaView style={{ flex: 1, marginTop: 20 }}>
-      <Text style={styles.texte}>Filtre nom</Text>
-      <BasicTextInput
-        setInput={setFiltreName}
-        placeholder="Ex: Test"
-        style={{ alignItems: "center" }}
-      />
-      <Text style={styles.texte}>Filtre tag</Text>
-      <BasicTextInput
-        setInput={handleTags}
-        placeholder="Ex: Test, Esteban, Maths"
-        style={{ alignItems: "center" }}
-      />
-      <Text style={styles.texte}>Filtre date</Text>
-      <BasicTextInput
-        setInput={setFiltreDate}
-        placeholder="2022"
-        style={{ alignItems: "center" }}
-      />
-      <Text style={styles.texte}>Notes</Text>
-      <CustomScrollView refresh={fetchData}>
-        <CollapseButton notes={filteredNotes}></CollapseButton>
-      </CustomScrollView>
-    </SafeAreaView>
-  );
+	return (
+		<SafeAreaView style={{ flex: 1, marginTop: 20 }}>
+			<Text style={styles.texte}>Filtre nom</Text>
+			<BasicTextInput
+				setInput={setFiltreName}
+				placeholder="Ex: Test"
+				style={{ alignItems: "center" }}
+			/>
+			<Text style={styles.texte}>Filtre tag</Text>
+			<BasicTextInput
+				setInput={handleTags}
+				placeholder="Ex: Test, Esteban, Maths"
+				style={{ alignItems: "center" }}
+			/>
+			<Text style={styles.texte}>Filtre date</Text>
+			<BasicTextInput
+				setInput={setFiltreDate}
+				placeholder="2022"
+				style={{ alignItems: "center" }}
+			/>
+			<Text style={styles.texte}>Notes</Text>
+			<CustomScrollView refresh={fetchData}>
+				<CollapseButton notes={filteredNotes}></CollapseButton>
+			</CustomScrollView>
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  texte: {
-    textAlign: "center",
-  },
+	texte: {
+		textAlign: "center",
+	},
 });
