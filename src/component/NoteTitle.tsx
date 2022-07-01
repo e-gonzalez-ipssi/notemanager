37cf2c3763cfcce7315823f71753069903c2ca47
NicolasDENIS <n.denis@ecole-ipssi.net>
2022-07-01 10:55:06 +0200

    @dev: Adding design

--- src/component/NoteTitle.tsx
+++ src/component/NoteTitle.tsx
@@ -4,49 +4,49 @@ import useTheme from "../hooks/theme";
 import { Note } from "../utils/note";
 
 export default function NoteTitle({ note }: { note: Note }) {
-    const { color } = useTheme();
+  const { color } = useTheme();
 
-    const image = note.image ? (
-        <Image
-            source={{ uri: note.image }}
-            style={{
-                width: 40,
-                height: 40,
-                borderRadius: 8,
-                marginLeft: 30,
-            }}
-        />
-    ) : (
-        <View
-            style={{
-                width: 40,
-                height: 40,
-                borderRadius: 8,
-                borderColor: color.primary,
-            }}
-        />
-    );
+  const image = note.image ? (
+    <Image
+      source={{ uri: note.image }}
+      style={{
+        width: 40,
+        height: 40,
+        borderRadius: 8,
+        marginLeft: 30,
+      }}
+    />
+  ) : (
+    <View
+      style={{
+        width: 40,
+        height: 40,
+        borderRadius: 8,
+        borderColor: color.primary,
+      }}
+    />
+  );
 
-    return (
-        <View
-            style={{
-                flexDirection: "row",
-                borderRadius: 8,
-                padding: 12,
-                flexWrap: "wrap"
-            }}
-        >
-            <Text
-                style={{
-                    fontSize: 30,
-                    color: color.text,
-                    marginLeft: 8,
-                    fontWeight: "900",
-                }}
-            >
-                {note.title}
-            </Text>
-            {image}
-        </View>
-    );
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        borderRadius: 8,
+        padding: 12,
+        flexWrap: "wrap",
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 30,
+          color: color.text,
+          marginLeft: 8,
+          fontWeight: "900",
+        }}
+      >
+        {note.title}
+      </Text>
+      {image}
+    </View>
+  );
 }
